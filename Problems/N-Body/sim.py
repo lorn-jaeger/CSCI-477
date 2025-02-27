b025ff849ba3f5ab03c91d5fd6c1309e91642a93
Lorn Jaeger <mail@lorn.us>
2025-02-26 20:43:52 -0700

    somewhat functioning RK45 implementation

--- Problems/N-Body/sim.py
+++ Problems/N-Body/sim.py
@@ -9,22 +9,51 @@ from IPython.display import HTML, display, clear_output
 # Simulation Functions
 #################################################
 
-def EulerRichardson(dt, f, t, y, args):
+def EulerRichardson(dt, f, t, y, *args):
     k1 = f(t, y, *args)
     y_mid = y + k1 * (dt/2)
     k2 = f(t + dt/2, y_mid, *args)
     return y + k2 * dt
 
-def RungeKutta4(dt, f, t, y, args):
+def RungeKutta4(dt, f, t, y, *args):
     k1 = f(t, y, *args)
     k2 = f(t + dt / 2, y + (dt / 2) * k1, *args)
     k3 = f(t + dt / 2, y + (dt / 2) * k2, *args)
     k4 = f(t + dt, y + dt * k3, *args)
     return y + (dt / 6) * (k1 + 2 * k2 + 2 * k3 + k4)
 
-def solve_ode(f,tspan, y0, method = EulerRichardson, *args, **options):
-    t0, tf = tspan
+def RungeKutta45(dt, f, t, y, *args):
+    c2, c3, c4, c5, c6, c6 = 1/5, 3/10, 4/5, 8/9, 1, 1
+    a21 = 1/5
+    a31, a32 = 3/40, 9/40
+    a41, a42, a43 = 44/45, -56/15, 32/9
+    a51, a52, a53, a54 = 19372/6561, -25360/2187, 64448/6561, -212/729
+    a61, a62, a63, a64, a65 = 9017/3168, -355/33, 46732/5247, 49/176, -5103/18656
+    a71, a72, a73, a74, a75, a76 = 35/384, 0, 500/1113, 125/192, -2187/6784, 11/84
+    b1, b2, b3, b4, b5, b6, b7 = 35/384, 0, 500/1113, 125/192, -2187/6784, 11/84, 0
+    bs1, bs2, bs3, bs4, bs5, bs6, bs7 = 5179/57600, 0, 7571/16695, 393/640, -92097/339200, 187/2100, 1/40
+
+    k1 = f(t, y, *args)
+    k2 = f(t + c2 * dt, y + dt * (a21 * k1), *args)
+    k3 = f(t + c3 * dt, y + dt * (a31 * k1 + a32 * k2), *args)
+    k4 = f(t + c4 * dt, y + dt * (a41 * k1 + a42 * k2 + a43 * k3), *args)
+    k5 = f(t + c5 * dt, y + dt * (a51 * k1 + a52 * k2 + a53 * k3 + a54 * k4), *args)
+    k6 = f(t + c6 * dt, y + dt * (a61 * k1 + a62 * k2 + a63 * k3 + a64 * k4 + a65 * k5), *args)
+    k7 = f(t + dt, y + dt * (a71 * k1 + a73 * k3 + a74 * k4 + a75 * k5 + a76 * k6), *args)
+
+    y_new = y + dt * (b1 * k1 + b2 * k2 + b3 * k3 + b4 * k4 + b5 * k5 + b6 * k6)
+    ys_new = y + dt * (bs1 * k1 + bs2 * k2 + bs3 * k3 + bs4 * k4 + bs5 * k5 + bs6 * k6 + bs7 * k7)
+
+    err = np.abs(y_new - ys_new)
+
+    return y_new, err
+
+def solve_ode(f, tspan, y0, method=RungeKutta45, *args, **options):
+   t0, tf = tspan
     dt = options.get('first_step', 0.01)
+    atol = options.get('atol', 1e-6)
+    rtol = options.get('rtol', 1e-6)
+    adaptive = method in [RungeKutta45]
 
     t = [t0]
     y = [y0]
@@ -33,10 +62,30 @@ def solve_ode(f,tspan, y0, method = EulerRichardson, *args, **options):
         y_c = y[-1]
         t_c = t[-1]
 
-        y_new = method(dt, f, t_c, y_c, args)
-        t_new = t_c + dt
-        y.append(y_new)
-        t.append(t_new)
+        if adaptive:
+            y_new, err = method(dt, f, t_c, y_c, *args)
+        else:
+            y_new = method(dt, f, t_c, y_c, *args)
+            err = np.zeros_like(y_c)
+
+        if adaptive:
+            scale = atol + rtol * np.abs(y_c)
+
+            scaled_err = np.sqrt(np.mean((err / scale) ** 2))
+
+            if scaled_err < 1.0:
+                t.append(t_c + dt)
+                y.append(y_new)
+
+                dt = dt * min(2.0, 0.9 * (1.0 / scaled_err) ** 0.2)
+            else:
+                dt = dt * max(0.5, 0.9 * (1.0 / scaled_err) ** 0.2)
+        else:
+            t.append(t_c + dt)
+            y.append(y_new)
+
+        if t[-1] + dt > tf:
+            dt = tf - t[-1]
 
     return np.array(t), np.array(y)
 
